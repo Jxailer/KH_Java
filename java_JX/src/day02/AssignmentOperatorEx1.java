43d4d64728433092c651bf1c5335d30f3f2ba5b1
Jxailer <89888717+Jxailer@users.noreply.github.com>
2023-12-11 12:45:01 +0900

    day02_assignment operator

---
+++ java_JX/src/day02/AssignmentOperatorEx1.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class AssignmentOperatorEx1 {
+
+	public static void main(String[] args) {
+		// 대입 연산자 예제
+		
+		int num1 = 1, num2 = 2;
+		num1 = 10;
+		
+//		10 = num1; // 10은 리터럴이기 때문에 값이 변하지 않음.
+		
+		int num3;
+		num3 = num1 + num2;
+//		num1 + num2 = num3; // 수식은 오른쪽으로 와야함.
+		System.out.println(num3);
+		
+		
+	}
+
+}
