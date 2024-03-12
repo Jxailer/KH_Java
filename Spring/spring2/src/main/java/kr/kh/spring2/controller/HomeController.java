cea62e98d9991ddda28491a1bdc34ce00f0d7b21
Jxailer <89888717+Jxailer@users.noreply.github.com>
2024-03-12 15:47:30 +0900

    Spring Mybatis DB 연동 테스트

---
+++ Spring/spring2/src/main/java/kr/kh/spring2/controller/HomeController.java
@@ -0,0 +1,26 @@
+package kr.kh.spring2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.spring2.model.vo.MemberVO;
+import kr.kh.spring2.service.MemberService;
+
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model, String id) {
+		MemberVO member = memberService.getMember(id);
+		System.out.println(member);
+		return "home";
+	}
+	
+}
