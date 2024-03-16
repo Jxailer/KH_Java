0d04168ecb4fb7889ed27ff408f7f8b870a3a7d4
Inseo Kang <JacksonXailer@gmail.com>
2024-03-16 12:02:52 +0900

    .

--- Spring/spring2/src/main/java/kr/kh/spring2/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package kr.kh.spring2.service;
-
-import kr.kh.spring2.model.vo.MemberVO;
-
-public interface MemberService {
-
-	MemberVO getMember(String id);
-
-}
---
+++ spring/spring/src/main/java/kr/kh/spring/controller/BoardController.java
@@ -0,0 +1,31 @@
+package kr.kh.spring.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.kh.spring.model.vo.BoardVO;
+import kr.kh.spring.pagination.Criteria;
+import kr.kh.spring.pagination.PageMaker;
+import kr.kh.spring.service.BoardService;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	private BoardService boardService;
+	
+	@GetMapping("/board/list")
+	public String boardList(Model model, Criteria cri) {
+		cri.setPerPageNum(5);
+		ArrayList<BoardVO> list = boardService.getBoardList(cri);
+		int totalCount = boardService.getBoardTotalCount(cri);
+		PageMaker pm = new PageMaker(3, cri, totalCount);
+		model.addAttribute("list", list);
+		model.addAttribute("pm", pm);
+		return "/board/list";
+	}
+}
