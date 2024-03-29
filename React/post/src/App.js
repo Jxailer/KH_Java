2d8b0316b9572e0e429f654de910890a0719b949
Jxailer <89888717+Jxailer@users.noreply.github.com>
2024-03-29 11:16:59 +0900

    4일차 cra 프로젝트 필요없는 코드 정리

--- React/post/src/App.js
+++ React/post/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// router를 사용하기 위해 설치하기
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from 'react';
+import {BrowserRouter, Route, Link, Routes } from 'react-router-dom'
+
+
+function App(){
+  
 }
+
 
 export default App;
