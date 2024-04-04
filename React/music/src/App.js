16f9353d2255fb7afde397d0733668f12cc734ec
Jxailer <89888717+Jxailer@users.noreply.github.com>
2024-04-04 15:14:49 +0900

    React 시험 문제

--- React/music/src/App.js
+++ React/music/src/App.js
@@ -4,27 +4,21 @@ import {BrowserRouter, Route, Link, Routes, useLocation, useNavigate } from 'rea
 
 function App() {
 
-  // 음악을 조회하고 등록하는 사이트를 구현하세요. 
-  /* 
-    음악 조회는 / 에서
-    = 음악 조회에서 삭제 버튼을 클릭하면 삭제되도록 구현(음악 번호 이용)
-  */
-  /*
-    음악 등록은 /insert에서
-    = 음악 등록시 음악번호(숫자), 제목, 가수, 장르를 입력하여 등록함 
-    = 음악 번호는 중복되지 않게 입력해서 추가
-  */
-
   let [list, setList] = useState([{
-    num : 1,
-    title : "ex1 title",
-    singer : "ex1 singer",
-    genre : "ex1 genre"
+    id : 1,
+    title : "Movie 1",
+    genre : "Drama",
+    release_date : "2022-01-01"
   },{
-      num : 2,
-      title : "ex2 title",
-      singer : "ex2 singer",
-      genre : "ex2 genre"
+      id : 2,
+      title : "Movie 2",
+      genre : "Action",
+      release_date : "2022-02-01"
+    },{
+      id : 3,
+      title : "Movie 3",
+      genre : "Comedy",
+      release_date : "2022-03-01"
     }
   ])
 
@@ -33,8 +27,8 @@ function App() {
   }
 
   // 기존 삭제 구현 방법과 비교하기
-  function remove(num){
-    let tmpList = list.filter(item=>item.num != num)  // 삭제하려는 음악 번호와 같지 않은 요소들만 가져와서 추가함
+  function remove(id){
+    let tmpList = list.filter(item=>item.id != id)  // 삭제하려는 음악 번호와 같지 않은 요소들만 가져와서 추가함
     setList(tmpList)
   }
 
@@ -54,8 +48,8 @@ function App() {
 function Nav(){
   return(
     <ul className="menu-list">
-      <li><Link to="/">음악 조회</Link></li>
-      <li><Link to="/insert">음악 추가</Link></li>
+      <li><Link to="/">List</Link></li>
+      <li><Link to="/insert">Add New Movie</Link></li>
     </ul>
   )
 }
@@ -71,32 +65,37 @@ function Home({list, remove, add}){
   }
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>제목</th>
-          <th>가수</th>
-          <th>장르</th>
-          <th>비고</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          list.map((item)=>{
-            return(
-            <tr key={item.num}>
-              <td>{item.title}</td>
-              <td>{item.singer}</td>
-              <td>{item.genre}</td>
-              <td>
-                <button onClick={()=>remove(item.num)}>X</button>
-              </td>
-            </tr>)
-            
-          })
-        }
-      </tbody>
-    </table>
+    <div className="container">
+      <h1>Movies</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Release Date</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            list.map((item)=>{
+              return(
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.genre}</td>
+                <td>{item.release_date}</td>
+                <td>
+                  <button onClick={()=>remove(item.id)}>Delete</button>
+                </td>
+              </tr>)
+              
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
   
 }
@@ -104,28 +103,53 @@ function Home({list, remove, add}){
 function Insert({list, add}){
   const navigate = useNavigate(); // 다른 페이지로 정보를 전송하기 위해 사용
   
-  let [num, setNum] = useState("")
+  let [id, setid] = useState("")
   let [title, setTitle] = useState("")
-  let [singer, setSinger] = useState("")
-  let [genre, setGenre] = useState("")
+  let [genre, setgenre] = useState("")
+  let [release_date, setrelease_date] = useState("")
 
-  const numChange = (e) => setNum(e.target.value)
+  const idChange = (e) => setid(e.target.value)
 
   function add(){
+    if(title == null || id == null || genre == null || release_date == null
+      || title.length == 0 || id.length == 0 || genre.length == 0 || release_date.length == 0){
+      alert("모든 값을 입력하세요")
+      return false;
+    }
+
+    let dup = false;
+
+    // 아이디 중복 확인
+    list.map((item)=>{
+      console.log("rooping")
+      if(item.id == id){
+        dup = true;
+      }
+      
+    })
+    
+    if(dup == true){
+      alert("이미 등록된 ID입니다. 다른 ID로 변경해주세요.")
+      return false;
+    }
+
     navigate("/",{
       state: {
-        num, title, singer, genre
+        id, title, genre, release_date
       }
     })
   }
 
   return(
-    <div>
-      <input placeholder='음악 번호' onChange={numChange}/>
-      <input placeholder='제목' onChange={(e)=>{setTitle(e.target.value)}}/>
-      <input placeholder='가수' onChange={(e)=>{setSinger(e.target.value)}}/>
-      <input placeholder='장르' onChange={(e)=>{setGenre(e.target.value)}}/>
-      <button onClick={()=>add()}>추가</button>
+    <div className="container">
+      <h1>Create movie</h1>
+      <input placeholder='Input movie id' onChange={idChange}/><br/>
+      <input placeholder='Input movie title' onChange={(e)=>{setTitle(e.target.value)}}/><br/>
+      <input placeholder='Input movie release_date' onChange={(e)=>{setgenre(e.target.value)}}/><br/>
+      <div>
+        출시일 : <input type="date" onChange={(e)=>{setrelease_date(e.target.value)}}/>
+      </div>
+      <button onClick={()=>add()}>add Movie</button>
     </div>
   )
 }
